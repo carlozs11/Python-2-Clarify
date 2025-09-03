46449afacf6a6ca698e1fdfbf77222d02bc68935
carlozs11 <carlozs11@hotmail.com>
2025-09-02 21:02:24 -0300

    Exercícios da primeira aula.
    Criação do primeiro exercício de consumo, seus arquivos de consultas, bases de dados e configurações.

---
+++ 00_PO.py
@@ -0,0 +1,22 @@
+class Carro:
+    def __init__(self, modelo, cor):
+        self.modelo = modelo
+        self.cor = cor
+        self.velocidade = 0 # o carro começa parado
+
+    def acelerar(self, incremento):
+            self.velocidade += incremento
+            print(f'O fusca {self.modelo} acelerou para {self.velocidade} Km/h.')
+
+    def desacelerar(self, decremento):
+         self.velocidade -= decremento
+         print(f'O {self.modelo} desacelerou para {self.velocidade} Km/h.')
+
+meu_carro = Carro('Montana', 'Prata', )
+outro_carro = Carro('Golf', 'Vermelho')
+
+# usando os metódos
+meu_carro.acelerar(20)
+meu_carro.acelerar(20)
+meu_carro.acelerar(20)
+meu_carro.desacelerar(10)
---
+++ 01_consumo.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, render_template_string
+import pandas as pd
+import sqlite3
+import plotly.express as px
+import plotly.io as pio
+import random
+import Config_PythonDeElite as config
+import consultas
+
+caminhoBanco = config.DB_PATH 
+pio.renderers.default = 'browser'
+nomeBanco = config.NOMEBANCO
+rotas = config.ROTAS
+tabelaA = config.TABELA_A
+tabelaB = config.TABELA_B
+
+# Arquivos a serem carregados
+
+dfDrinks = pd.read_csv(f'{caminhoBanco} {tabelaA}')
+dfAvengers = pd.read_csv(f'{caminhoBanco}{tabelaB}', encoding='latin1')
+
+# outros exemplos de encodings: utf-8, cp1256, iso 8859-1
+
+# criamos o banco de dados em SQL caso não exista
+conn = sqlite3.connect(f'{caminhoBanco} {nomeBanco}')
+
+dfDrinks.to_sql("bebidas", conn, if_exists="replace", index=False)
+dfAvengers.to_sql("vingadores", conn, if_exists="replace", index=False)
+conn.commit()
+conn.close()
+
+html_template = f'''
+ <h1> Dashboards - Consumo de Alcool </h1>
+    <h2> Parte 01 </h2>
+        <ul>
+            <li><a href="/grafico1"> Top 10 paises em consumo de alcool </a></li>
+            <li><a href="/grafico2"> Media de consumo por Tipo </a></li>
+            <li><a href="/grafico3"> Consumo total por Região </a></li>
+            <li><a href="/grafico4"> Comparativo entre tipos de bebidas </a></li>
+            <li><a href="/pais"> Insights por pais </a></li>
+        </ul>
+    <h2> Parte 02 </h2>
+        <ul>
+            <li><a href="/comparar"> Comparar </a></li>
+            <li><a href="/upload"> Upload CSV Vingadores </a></li>
+            <li><a href="/apagar"> Apagar Tabela </a></li>
+            <li><a href="/ver"> Ver Tabela </a></li>
+            <li><a href="/vaa"> V.A.A (Vingadores Alcolicos Anonimos) </a></li>
+        </ul>
+   '''
+
+# iniciar o flask
+
+app = Flask(__name__)
+
+@app.route(rotas[0])
+def index():
+    return render_template_string("html_template")
+
+@app.route(rotas[1])
+def grafico1():
+    with sqlite3.connect(f'{caminhoBanco} {nomeBanco}') as conn:
+        df = pd.read_sql_query(consultas.consulta01, conn)
+    figuraGrafico1 = px.bar(
+        df,
+        x = 'country',
+        y = 'total_litres_of_pure_alcohol',
+        title = 'Top 10 países em consumo de alcool'
+    )
+    return figuraGrafico1.to_html()
+# inica o servidor 
+
+if __name__ == '__main__':
+    app.run(debug=True)
